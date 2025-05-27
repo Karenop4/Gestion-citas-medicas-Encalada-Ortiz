222ed16386fb65a6d37660381c6dfebdadaf1e54
Andrés Encalada <encaladaandres15@gmail.com>
2025-05-26 21:10:06 -0500

    Transiciones
    Se implementaron animaciones de transición entre las páginas y se corrigió un error que se presentaba con las imagenes.

--- gestion_citas_medicas/src/app/app.config.ts
+++ gestion_citas_medicas/src/app/app.config.ts
@@ -1,7 +1,7 @@
 // src/app/app.config.ts
 
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
@@ -9,7 +9,7 @@ import { firebaseConfig } from '../environments/firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
   ]
