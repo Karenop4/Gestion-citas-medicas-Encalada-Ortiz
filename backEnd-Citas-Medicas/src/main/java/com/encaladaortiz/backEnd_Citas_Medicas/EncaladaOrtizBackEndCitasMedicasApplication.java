7db680df7b13fa9579b5c8d5d8331a0de909b2ff
Karen Ortiz <karenorpe2004@gmail.com>
2025-06-17 07:50:20 -0500

    Backend
    creación proyecto en Spring boot para el backend

---
+++ backEnd-Citas-Medicas/src/main/java/com/encaladaortiz/backEnd_Citas_Medicas/EncaladaOrtizBackEndCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.encaladaortiz.backEnd_Citas_Medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncaladaOrtizBackEndCitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncaladaOrtizBackEndCitasMedicasApplication.class, args);
+	}
+
+}
---
+++ backEnd-Citas-Medicas/src/test/java/com/encaladaortiz/backEnd_Citas_Medicas/EncaladaOrtizBackEndCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encaladaortiz.backEnd_Citas_Medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncaladaOrtizBackEndCitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
